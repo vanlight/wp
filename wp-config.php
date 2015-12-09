23ec6cb65e14dce4cc10a3876db3d05b33d1cd4f
vanlight <van35@gmail.com>
2015-12-10 00:53:51 +0700

    test

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,8 @@
  *
  * @package WordPress
  */
-
+define('WP_HOME','http://xemwebsite.tk');
+define('WP_SITEURL','http://xemwebsite.tk');
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'u196318776_wp');
