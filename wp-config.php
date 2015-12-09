cec31f7c9973e93db6faf52f9c383cbdfdbd651b
vanlight <van35@gmail.com>
2015-12-10 00:33:58 +0700

    test

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp');
+define('DB_NAME', 'u196318776_wp');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u196318776_root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'admin#abcx');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.hostinger.vn');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
